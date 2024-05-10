9626c64fa1517373480b6718aa61d09f1279f9fd
Arlin Perez <arlinblack01@gmail.com>
2024-05-10 12:43:05 -0400

    post-section.spec.js: all test cases added to its test suite

---
+++ tests/shrinkit/homepage/blog-page/all-post-page/posts/posts-section.spec.js
@@ -0,0 +1,61 @@
+const { test, expect, chromium } = require('@playwright/test')
+
+test.describe('Post Section Suite', () => {
+    
+    test('verify that all the navigation bar elements are working properly', async ({ page }) => {
+        
+    })
+    
+    test('verify that post section has a "Back to Blog" link button', async ({ page }) => {
+        
+    })
+
+    test('verify that link button has the text: "Back to Blog"', async ({ page }) => {
+        
+    })
+
+    test('verify that the "Back to Blog" link button takes you to the "Blog" page', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section has any of these categoies: "Announcements/Features/Tutorials/Releases/Guides"', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section has valid date', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section has a title', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section has a descriptive subtitle', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section has a static image', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section has the "Posted by" phrase', async ({ page }) => {
+        
+    })
+
+    test('verify that the post section shows the "Author" avatar picture', async ({ page }) => {
+        
+    })
+
+    test('verify that the post has a section called: "Related reading"', async ({ page }) => {
+        
+    })
+
+    test('verify that the post has a "Try Shrinkit!" download button that takes you to the app store', async ({ page }) => {
+        
+    })
+
+    test('verify that all footer elements are working as expected', async ({ page }) => {
+        
+    })
+
+})
